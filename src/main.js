c33ff676c58ade68d83d8f1b34be0ce8a6aa4586
xiayoutao <1425244634@qq.com>
2020-11-27 18:52:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
-import vant from 'vant';
+import vant, { Dialog } from 'vant';
 import 'vant/lib/index.css';
 
 const app = createApp(App);
-// app.config.globalProperties.$alert = Dialog.alert;
-// app.config.globalProperties.$confirm = Dialog.confirm;
+app.config.globalProperties.$alert = Dialog.alert;
+app.config.globalProperties.$confirm = Dialog.confirm;
 app.use(vant);
 app.use(router);
 app.mount('#app');
